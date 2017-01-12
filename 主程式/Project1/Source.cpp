fde7ef5bbe49615b2bb7242b38511a96190a2193
apple860912 <love442727@gmail.com>
2017-01-12 17:56:13 +0800

    6
    fight!!

--- 主程式/Project1/Source.cpp
+++ 主程式/Project1/Source.cpp
@@ -13,15 +13,11 @@
 #define true     1
 #define false    0
 
-
 typedef struct XY_str
 {
 	int x;
 	int y;
-}XY_str;   
-
-XY_str sBody[2500]; 
-
+}XY_str;     XY_str sBody[2500]; // range = 20*50 = 1000
 
 void gotoxy(int xpos, int ypos)
 
@@ -36,7 +32,6 @@ void gotoxy(int xpos, int ypos)
 	SetConsoleCursorPosition(hOuput, scrn); //設定游標位置
 
 }
-
 void showCursor(int visible)
 {
 	CONSOLE_CURSOR_INFO ConCurInf;
@@ -52,7 +47,6 @@ void showCursor(int visible)
 	SetConsoleCursorInfo(hStdOut, &ConCurInf);
 
 }
-
 void Snake(int bodylenhgt, int foodcolor)
 {
 	int color[2000] = { 0 };
@@ -76,22 +70,17 @@ void Snake(int bodylenhgt, int foodcolor)
 
 	}
 }
-
-void printWall(){
-
+void Wall()
+{
 	int i, j;
-
-
-	for (i = 0; i<length; i++){
-
-		for (j = 0; j<width + 1; j += 2){
-
+	for (i = 0; i<length; i++)
+	{
+		for (j = 0; j<width + 1; j += 2)
+		{
 			if (i == 0 || i == length - 1 || j == 0 || j == width)
 			{
 				gotoxy(j, i);
-				_setmode(_fileno(stdout), _O_U16TEXT);   //使用Unicode
 				wprintf(L"■");
-
 			}
 
 		}
@@ -99,26 +88,74 @@ void printWall(){
 	}
 
 }
-
-void printSnake(int i){
-
-	gotoxy(sBody[i].x, sBody[i].y);
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | 0X0e);
-	_setmode(_fileno(stdout), _O_U16TEXT);   //使用Unicode
-	wprintf(L"■");
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | 0X0f);
+void Information(int food, int speed)
+{
+	gotoxy(width + 4, 7);
+	wprintf(L"Use P to pause.");
+	gotoxy(width + 4, 9);
+	wprintf(L"Grade: %d", food * 10);
+	gotoxy(width + 4, 11);
+	wprintf(L"Speed: %3d            ", 97 - speed);
 }
-
-void setSite(int i, int site_x, int site_y){
-
+void setSite(int i, int site_x, int site_y)
+{
 	sBody[i].x = site_x;
-
 	sBody[i].y = site_y;
-
 }
+void setStartSite()
+{
+	int i;
+	sBody[0].x = sBody[1].x = sBody[2].x = sBody[3].x = sBody[4].x = 2;
+	for (i = 0; i<5; i++) sBody[i].y = abs(5 - i);
+}
+
+
+
 
 int main()
 {
+	_setmode(_fileno(stdout), _O_U16TEXT);   //使用Unicode
+	XY_str foodSite, coor, last;
 
+	int startBodyLenght = 5, startFood = 0, bodyLenght, Food;
+	int keyinFirst, keyinSecond;
+	int i, j, gameOver = false, FoodAppear = false, xyChanged = false;
+	int path = 2; // 方向
+	int SnakeSpeed = 100;
+	int foodcolor;
+	char gameKey = 'y';
+	srand(time(NULL));
+	showCursor(0);
+
+	foodcolor = (rand() % 4) + 1;
+
+	while (1)
+	{
+
+		if (gameKey == 'n' || gameKey == 'N') break;
+
+		setStartSite();
+		Wall();
+		bodyLenght = startBodyLenght;
+		Food = startFood;
+		SnakeSpeed = 100;
+		coor.x = 2;
+		coor.y = 4;
+		path = 2;
+		FoodAppear = false;
+		gameOver = false;
+		xyChanged = false;
+		Information(startFood, SnakeSpeed);
+		Snake(bodyLenght, foodcolor);
+
+		do{
+			gotoxy(width + 4, 13);
+			wprintf(L"Enter to start.");
+			gameKey = _getch();
+			gotoxy(width + 4, 13);
+			wprintf(L"               ");
+		} while (gameKey != 13);
+
+	}
 	return 0;
 }
